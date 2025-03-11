cb1c37a82d760812e99221b97be7072c9dc82566
veronica <veronicasliman5@gmail.com>
2025-03-11 15:43:00 +0200

    veronica's intities

---
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/Dietitian.java
@@ -0,0 +1,46 @@
+package il.cshaifasweng.OCSFMediatorExample.entities;
+
+import jakarta.persistence.*;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.PrimaryKeyJoinColumn;
+import java.util.List;
+
+@Entity
+@Table(name = "dietitians")
+@PrimaryKeyJoinColumn(name = "user_id")
+public class Dietitian extends User {
+
+    @OneToMany(mappedBy = "dietitian")
+    private List<Item> items;
+
+    // Constructors
+    public Dietitian() {}
+
+    public Dietitian(String name, int phoneNumber, String email, String password) {
+        super(name, phoneNumber, email, password);
+    }
+
+    // Methods for item management
+    public void changeItemPrice(Item item, double newPrice) {
+        item.setPrice(newPrice);
+    }
+
+    public void changeItemName(Item item, String newName) {
+        item.setName(newName);
+    }
+
+    public void deleteItem(Item item) {
+        items.remove(item);
+    }
+
+    public void sendRequest(Item item) {
+        System.out.println("Sending request for item: " + item.getName());
+    }
+
+    // Getters and Setters
+    public List<Item> getItems() { return items; }
+    public void setItems(List<Item> items) { this.items = items; }
+}
+
---
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/Hostess.java
@@ -0,0 +1,23 @@
+package il.cshaifasweng.OCSFMediatorExample.entities;
+
+import jakarta.persistence.*;
+
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+
+@Entity
+@Table(name = "hostesses")
+@PrimaryKeyJoinColumn(name = "worker_id")
+public class Hostess extends Worker implements Reservation {
+
+    @Override
+    public void reserveTable(int capacity) {
+        System.out.println("Reserving a table for " + capacity + " guests.");
+    }
+
+    @Override
+    public void freeTable(Table table) {
+        System.out.println("Table " + table.getId() + " is now free.");
+    }
+}
+

57372413f147bc47618182fee8fecaa38c01ce3e
tahazoabi1 <tzoabi890@gmail.com>
2025-03-13 16:18:58 +0200

    changes in MenuItem

--- entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/MenuItem.java
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/MenuItem.java
@@ -1,9 +1,3 @@
-
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package il.cshaifasweng.OCSFMediatorExample.entities;
 
 import java.io.Serializable;
@@ -18,41 +12,38 @@ import javax.persistence.Table;
 @Table(name = "menu_items")
 public class MenuItem implements Serializable {
     private static final long serialVersionUID = 1L;
+
     @Id
-    @GeneratedValue(
-            strategy = GenerationType.IDENTITY
-    )
-    @Column(
-            name = "id"
-    )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
-    @Column(
-            name = "name"
-    )
+
+    @Column(name = "name")
     private String name;
-    @Column(
-            name = "ingredients"
-    )
+
+    @Column(name = "ingredients")
     private String ingredients;
-    @Column(
-            name = "preferences"
-    )
+
+    @Column(name = "preferences")
     private String preferences;
-    @Column(
-            name = "price"
-    )
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "price")
     private double price;
 
-    public MenuItem() {
-    }
+    public MenuItem() {}
 
-    public MenuItem(int id, String name, String ingredients, String preferences,Double price) {
+    public MenuItem(int id, String name, String ingredients, String preferences, double price) {
+        this.id = id;
         this.name = name;
         this.ingredients = ingredients;
         this.preferences = preferences;
         this.price = price;
     }
 
+    // Getters and Setters
     public int getId() {
         return this.id;
     }
@@ -64,39 +55,45 @@ public class MenuItem implements Serializable {
     public String getName() {
         return this.name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setTitle(String name) {
-        this.name = name;
-    }
-
-    public String getingredients() {
+    public String getIngredients() {
         return this.ingredients;
     }
 
-    public void setingredients(String ingredients) {
+    public void setIngredients(String ingredients) {
         this.ingredients = ingredients;
     }
 
-    public void setpreferences(String preferences) {
-        this.preferences = preferences;
-    }
-
-    public String getpreferences() {
+    public String getPreferences() {
         return this.preferences;
     }
 
-    public Double getPrice() {
+    public void setPreferences(String preferences) {
+        this.preferences = preferences;
+    }
+
+    public String getImageUrl() {
+        return this.imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public double getPrice() {
         return this.price;
     }
 
-    public void setPrice(double Price) {
-        this.price = Price;
+    public void setPrice(double price) {
+        this.price = price;
     }
+
+    @Override
     public String toString() {
-        return String.format("MenuItem ID: %d\nTitle: %s\ningredients: %s\npreferences: %s%s\nprice: %s", this.id, this.name, this.ingredients, this.preferences, this.price);
+        return String.format("MenuItem ID: %d\nName: %s\nIngredients: %s\nPreferences: %s\nPrice: %.2f", this.id, this.name, this.ingredients, this.preferences, this.price);
     }
 }
-
